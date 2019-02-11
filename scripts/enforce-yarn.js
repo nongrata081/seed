5802eca7f9b02e0cda601ee2effa5197c014a957
Vahan Minasian <vaanatwork@gmail.com>
2019-02-11 17:33:59 +0100

    feat(enforce-yarn): enforce yarn instead of npm on install of packages

---
+++ scripts/enforce-yarn.js
@@ -0,0 +1,8 @@
+/**
+ * Restrict using `npm` for installing packages
+ */
+if (process.env.npm_execpath.indexOf('yarn') === -1) {
+    console.error('You must use Yarn to install dependencies:');
+    console.error('  $ yarn install');
+    process.exit(1);
+}
